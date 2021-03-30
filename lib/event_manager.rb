c68b99e6dc249a0621fcbf6fdd28ca9af1d6babe
Yulun Chen <yulunchen25@gmail.com>
2021-03-30 03:02:06 -0400

    Completed Iteration 1

---
+++ lib/event_manager.rb
@@ -0,0 +1,13 @@
+require 'csv'
+puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
+end
